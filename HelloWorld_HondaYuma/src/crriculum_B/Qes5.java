7922e8ef40a6b3d32f30b606d3720857cbdb70c9
Guest_11 <Guest_11@DESKTOP-GA2OUK7>
2025-05-02 19:00:46 +0900

    課題１６ー６途中まで

--- HelloWorld_HondaYuma/src/crriculum_B/Qes5.java
+++ HelloWorld_HondaYuma/src/crriculum_B/Qes5.java
@@ -2,6 +2,14 @@ package crriculum_B;
 
 public class Qes5 {
 	public static void main(String[] args) {
-		
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 20; j++) {
+				System.out.printf("%03d * %03d = %03d", j, i, i*j);
+				if(j != 20) {
+					System.out.print(" || ");
+				}
+			}
+			System.out.println("\n");
+		}
 	}
 }
--- HelloWorld_HondaYuma/src/crriculum_B/Qes6.java
+++ HelloWorld_HondaYuma/src/crriculum_B/Qes6.java
@@ -1,7 +1,26 @@
 package crriculum_B;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+
 public class Qes6 {
 	public static void main(String[] args) {
+		String[] goods = {"パソコン","冷蔵庫","扇風機","洗濯機","加湿器","テレビ","ディスプレイ"}; 
+		System.out.println("在庫の知りたい商品名を入力してください。「、」で区切る");
 		
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.next();
+		
+		//入力された文章を「、」で区切る
+		List<String> request = new ArrayList<String>();
+        for (String a : str.split("、")) {
+            request.add(a);
+        }
+        //リクエストのあった商品に重複があれば省く
+        List<String> listWithoutDuplicates = new ArrayList<String>(new HashSet<>(request));
+        System.out.println(listWithoutDuplicates);
+        
 	}
 }
