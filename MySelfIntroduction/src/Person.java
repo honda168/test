44f3a9aa908a9ceb74ef1d9f532969b1e81b2409
Guest_11 <Guest_11@DESKTOP-GA2OUK7>
2025-05-08 10:18:15 +0900

    30.自己紹介プログラムを作成①

---
+++ MySelfIntroduction/src/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+	String name;
+	int age;
+	double height;
+	double weight;
+	
+	static int count;
+	
+	public Person(String name, int age, double height, double weight) {
+		this.name = name;
+		this.age = age;
+		this.height = height;//身長
+		this.weight = weight;//体重
+		count++;
+	}
+	
+	public double getBmi(double height, double weight) {
+		double bmi = weight / (height * 2);
+		bmi = Math.round(bmi * 100.0) / 100.0;
+		return bmi;
+		
+	}
+	public void print() {
+	    System.out.println("名前は" + name + "です。");
+	    System.out.println("年齢は" + age + "です。");
+	    System.out.println("BMIは" + getBmi(height, weight) + "です。");
+	    System.out.println();
+	}
+}
